81ab6a02a0913e87c3eb042457dc23199e09098d
satirg <149798564+satirg@users.noreply.github.com>
2024-02-07 02:39:30 +0500

    Update scripts.js

--- module5-solution/js/scripts.js
+++ module5-solution/js/scripts.js
@@ -29,7 +29,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoryHtml = "snippets/category-snippet.html";
   var menuItemsUrl =
-    "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+    "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/{category_short_name}.json";
   var menuItemsTitleHtml = "snippets/menu-items-title.html";
   var menuItemHtml = "snippets/menu-item.html";
   
