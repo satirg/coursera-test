9084cff8618b2a3b617ea7ddd9b50ee18401a369
satirg <149798564+satirg@users.noreply.github.com>
2024-02-07 02:30:39 +0500

    Add files via upload

--- module5-solution/js/scripts.js
+++ module5-solution/js/scripts.js
@@ -25,11 +25,11 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   
   var homeHtml = "snippets/home-snippet.html";
   var allCategoriesUrl =
-    "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
+    "https://davids-restaurant.herokuapp.com/categories.json";
   var categoriesTitleHtml = "snippets/categories-title-snippet.html";
   var categoryHtml = "snippets/category-snippet.html";
   var menuItemsUrl =
-    "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
+    "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
   var menuItemsTitleHtml = "snippets/menu-items-title.html";
   var menuItemHtml = "snippets/menu-item.html";
   
