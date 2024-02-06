4474f43e0835991325836d2994fb77cafd22dc9a
satirg <149798564+satirg@users.noreply.github.com>
2024-02-07 02:13:57 +0500

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -20,8 +20,8 @@ var categoriesTitleHtml = "snippets/categories-title-snippet.html";
 var categoryHtml = "snippets/category-snippet.html";
 var menuItemsUrl = 
   "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
-var menuItemsTitleHtml = "snippets/menu-items-title.html";
-var menuItemHtml = "snippets/menu-item.html";
+var menuItemsTitleHtml = "snippets/categories-title-snippet.html";
+var menuItemHtml = "snippets/category-snippet.html;
 
 // Convenience function for inserting innerHTML for 'select'
 var insertHtml = function (selector, html) {
